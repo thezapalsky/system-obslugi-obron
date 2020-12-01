58a400e5cbd4bfd1bab9d85b7da43d6a0aa8998a
zapalski@student.agh.edu.pl <zapalski@student.agh.edu.pl>
2020-12-01 16:55:58 +0100

    boundary conditions testing

---
+++ UnitTestProject1/BoundaryConditionsTesting.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemObslugiObron;
+
+namespace UnitTestProject1
+{
+    public class BoundaryConditionsTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
